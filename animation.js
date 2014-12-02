306e41720ade7a962812f8841b44396f0b5017b3
quody <aleksi.o.jokela@aalto.fi>
2014-12-02 17:33:52 +0200

    ujoo testailua

--- animation.js
+++ animation.js
@@ -3,6 +3,32 @@ var stage, pauseCircle, goCircle, output;
 		function init() {
 			stage = new createjs.Stage("game");
 			
+			//Luodaan stageen "maailma", joka sisältää kaikki siinä olevat objektit
+			world = new createjs.Container();
+			//Järjestellään objektit layereihin.
+			//"World layer 1" Tippuvat kamat. (jää nyt tyhjäksi)
+			wl1 = new createjs.Container();
+			//"World layer 2". Pelaajaobjektit (eli oikeesti vaan yks jäbä)
+			wl2 = new createjs.Container();
+			//Lisätään containerit maailmaa ja maailma stageen
+			world.addChild(wl1);
+			world.addChild(wl2);
+			stage.addChild(world);
+			
+			//Testataan ladata pelaajasprite ja heittää se ruudulle.. lopullinen versio voisi olla eligantimpi ja enemmän kamaa player.jsssä
+			//Lisää EaselJS spritesheeteistä http://createjs.com/Docs/EaselJS/classes/SpriteSheet.html
+			 var data = {
+			 	//Framerate animaatiolle, myös yksittäisille actioneille (kuten climb) voi vielä asettaa oman nopeuden
+			 	 framerate:10,
+				 images: ["climber.png"],
+				 frames: {width:35, height:50},
+				 animations: {climb:[0,4]}
+			 };
+			 var spriteSheet = new createjs.SpriteSheet(data);
+			 var player = new createjs.Sprite(spriteSheet, "climb");
+			player.x=10;
+			player.y=10;
+			wl2.addChild(player);
 			//stage.addChild(goCircle);
 
 			// and register our main listener
